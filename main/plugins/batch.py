d968b93931b6855efffd5b0b319e6aa55dd5be80
GITPALPRO <89265363+GITPALPRO@users.noreply.github.com>
2022-07-01 14:23:44 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -81,25 +81,25 @@ async def run_batch(userbot, client, sender, link, _range):
         timer = 60
         if i < 200:
             timer = 1
-        if i < 200 and i > 400:
+        if i < 400 and i > 200:
             timer = 2
-        if i < 400 and i > 600:
+        if i < 600 and i > 400:
             timer = 3
-        if i < 600 and i > 800:
+        if i < 800 and i > 600:
             timer = 4
-        if i < 800 and i > 1000:
+        if i < 1500 and i > 800:
             timer = 5
         if not 't.me/c/' in link:
             if i < 200:
-              timer = 1
-            if i < 200 and i > 400:
-              timer = 2
-            if i < 400 and i > 600:
-              timer = 3
-            if i < 600 and i > 800:
-              timer = 4
-            if i < 800 and i > 1000:
-              timer = 5
+                timer = 1
+            if i < 400 and i > 200:
+                timer = 2
+            if i < 600 and i > 400:
+                timer = 3
+            if i < 800 and i > 600:
+                timer = 4
+            if i < 1500 and i > 800:
+                timer = 5
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
